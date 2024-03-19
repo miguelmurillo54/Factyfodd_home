22863ae3c8c78e28d158cabaf703d8867f0370f1
miguel <mmurillo54@uan.edu.co>
2024-03-19 12:40:42 -0500

    Inicio desarrollo de proyecto

--- app/src/main/java/com/example/factyfodd_home/NuevasRecetas.kt
+++ app/src/main/java/com/example/factyfodd_home/NuevasRecetas.kt
@@ -1,23 +1,21 @@
 package com.example.factyfodd_home
 
-import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import android.widget.ImageButton
 
-class MainActivity : AppCompatActivity() {
-
+class NuevasRecetas : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_nuevas_recetas)
 
-        val btn: ImageButton= findViewById(R.id.imageButtonnewrecipe)
+        val btn: ImageButton= findViewById(R.id.imageButtonnewrecipeatras)
         btn.setOnClickListener{
 
-            val intent: Intent=Intent(this, NuevasRecetas:: class.java)
+            val intent: Intent=Intent(this,MainActivity ::class.java)
             startActivity(intent)
         }
     }
+
 }
