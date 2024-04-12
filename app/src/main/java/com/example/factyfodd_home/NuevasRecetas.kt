a1407a7082263af83fb233a042c85878b9c977ba
miguel <mmurillo54@uan.edu.co>
2024-04-12 11:00:16 -0500

    añade nuevas recetas falta ordenar

--- app/src/main/java/com/example/factyfodd_home/NuevasRecetas.kt
+++ app/src/main/java/com/example/factyfodd_home/NuevasRecetas.kt
@@ -22,6 +22,37 @@ class NuevasRecetas : AppCompatActivity() {
             val intent: Intent=Intent(this,newrecipeuno ::class.java)
             startActivity(intent)
         }
+        val btnnewrecipedos: ImageButton= findViewById(R.id.imageButtontoast)
+        btnnewrecipedos.setOnClickListener{
+
+            val intent: Intent=Intent(this,newrecipedos ::class.java)
+            startActivity(intent)
+        }
+        val btnnewrecipetres: ImageButton= findViewById(R.id.imageButtonpasteltaza)
+        btnnewrecipetres.setOnClickListener{
+
+            val intent: Intent=Intent(this,newrecipetres ::class.java)
+            startActivity(intent)
+        }
+        val btnnewrecipecuatro: ImageButton= findViewById(R.id.imageButtonpancalabacin)
+        btnnewrecipecuatro.setOnClickListener{
+
+            val intent: Intent=Intent(this,newrecipecuatro ::class.java)
+            startActivity(intent)
+        }
+        val btnnewrecipecinco: ImageButton= findViewById(R.id.imageButtonalitas)
+        btnnewrecipecinco.setOnClickListener{
+
+            val intent: Intent=Intent(this,newrecipecinco ::class.java)
+            startActivity(intent)
+        }
+        val btnnewrecipeseis: ImageButton= findViewById(R.id.imageButtonalbondigas)
+        btnnewrecipeseis.setOnClickListener{
+
+            val intent: Intent=Intent(this,newrecipeseis ::class.java)
+            startActivity(intent)
+        }
+
     }
 
 }
